310e5ab3a6b3b9a8719f601b8087daf9204195dd
Taras Shpek <taras.shpek@gmail.com>
2025-06-23 15:22:58 +0300

    update <parent> in pom.xml

--- src/test/java/software/plusminus/test/SeleniumTestTest.java
+++ src/test/java/software/plusminus/test/SeleniumTestTest.java
@@ -1,10 +1,17 @@
 package software.plusminus.test;
 
 import org.junit.Test;
+import software.plusminus.selenium.model.WebTestOptions;
 
 import static software.plusminus.check.Checks.check;
 
 public class SeleniumTestTest extends SeleniumTest {
+
+    @Override
+    protected WebTestOptions options() {
+        return super.options()
+                .logsFilter(log -> !log.getMessage().contains("favicon"));
+    }
 
     @Override
     protected String url() {
