f9d31ab80b8459b463f1c3ccf8329dd83c346a26
Taras Shpek <taras.shpek@gmail.com>
2025-12-30 12:53:54 +0200

    Extend TestTransactionHelper

--- src/main/java/software/plusminus/test/helpers/data/TestTransactionHelper.java
+++ src/main/java/software/plusminus/test/helpers/data/TestTransactionHelper.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 import software.plusminus.test.helpers.TestHelper;
 
+import java.util.concurrent.Callable;
 import javax.transaction.Transactional;
 
 @ConditionalOnClass(Transactional.class)
@@ -13,5 +14,14 @@ public class TestTransactionHelper implements TestHelper {
     @Transactional
     public void run(Runnable run) {
         run.run();
+    }
+
+    @Transactional
+    public <T> T call(Callable<T> call) {
+        try {
+            return call.call();
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
     }
 }
